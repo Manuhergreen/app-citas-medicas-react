b558e3b6ccf0cc31fd78df1f917a22b18a4968f4
Manuhergreen <manuhernandezbi@gmail.com>
2023-08-21 10:46:05 +0200

    first commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,25 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-sm px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Manu</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">correo@correo.es</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">10/08/23</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In imperdiet bibendum purus, non elementum ipsum tincidunt sit amet. Morbi sollicitudin sed sem vel pharetra. Curabitur faucibus felis massa. Maecenas non nibh tempus libero bibendum luctus. Vivamus leo urna, placerat et suscipit id, ultrices iaculis nisi. Praesent ultricies vitae diam ornare ultricies. Donec condimentum, ligula sit amet consequat lacinia, massa odio ullamcorper odio, vel sollicitudin neque libero sed augue. Ut sagittis tincidunt purus ut posuere. </span>
+            </p>
+        </div>
+  )
+}
+
+export default Paciente
